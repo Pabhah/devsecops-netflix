d7a12787ffd3a0c2706e0fd30f8a99151cbc5d04
Pabhah <pavanp4p@gmail.com>
2025-05-22 00:49:59 +0530

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "devsecops-netflix-reyaz-1" # Replace with your actual S3 bucket name
+    bucket = "devsecops-netflix-pabhah" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
-    region = "ap-south-1"
+    region = "ap-northeast-1a"
   }
 }
